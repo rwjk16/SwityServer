be9843fbf59db34e87e9681a32ac7e22bc0799dd
Russell Weber <rwjk16@gmail.com>
2019-06-17 14:20:43 -0400

    for troubleshooting

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,11 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+  
+  rrouter.get("upcase", String.parameter) { req -> String in
+    let id = try req.parameters.next(String.self)
+    return database.get(id: id)
+  }
 
     // Example of configuring a controller
     let todoController = TodoController()
